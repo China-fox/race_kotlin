41cb15cb700b96cfaa892b94fffb9a91a00b2bea
China fox <55652840+China-fox@users.noreply.github.com>
2020-03-15 05:05:06 +0900

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,23 +1,30 @@
 package kot
 
 import race.MyThread
+import java.util.logging.LogManager
+import java.util.logging.Logger
 
 @Volatile var winner: String = ""
+private val logger = Logger.getLogger(MyThread::class.java.name)
 
 fun main() {
-
+    LogManager.getLogManager().readConfiguration(
+            MyThread::class.java.getResourceAsStream("/logging.properties"))
     val threadA = MyThread("Первый")
     val threadB = MyThread("Второй")
-  //  val threadC = MyThread("Третий")
     threadA.join()
     threadB.join()
-  //  threadC.join()
-    println("Победитель - $winner")
+    writeLog("Победитель - $winner")
+
 
 }
 
-fun whoWinner(name: String){
+ fun whoWinner(name: String){
     if (winner.isEmpty()) {
         winner = name
     }
+}
+
+fun writeLog(text: String) {
+    logger.info(text)
 }
